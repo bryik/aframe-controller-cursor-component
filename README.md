75a7bacf26855a41c200fa915c13b6174666d43f
bryik <ben@wsundine.com>
2016-07-22 17:12:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+## aframe-vive-cursor-component
+
+    Note: This component only works with the master branch of A-Frame (soon to be released as 0.3.0).
+
+This is an [A-Frame](https://aframe.io) component for adding a cursor to an HTC Vive controller.
+
+### API
+
+| Property |          Description          | Default Value |
+|:--------:|:-----------------------------:|:-------------:|
+|   color  |          Laser color          |    0x0000ff   |
+|  radius  | Laser radius (top and bottom) |     0.001     |
+
+### Usage
+
+Simply attach the component to an entity alongside the vive-controls component.
+
+```html
+    <a-entity vive-controls="hand: right" vive-cursor></a-entity>
+```
+
+As it is based on [A-Frame's built-in cursor](https://aframe.io/docs/master/components/cursor.html), many of the events and states are shared.
+
+Note that "mouseup" and "mousedown" have been mapped to the trigger. In other words, pulling the trigger is the same as clicking the right-mouse button.
+
+### Notes
+
+To avoid intersecting with the laser, the raycaster's "near" property is set to 0.03. As a result, you may see odd results when the top of the controller is held extremely close to the entity you wish to intersect.
+
+Generally, this component assumes the Vive controller model. If you are using the hand model or a different model entirely, you may need to change the position of the laser.
+
+#### Browser
+
+Install and use by directly including the [browser files](dist):
+
+```html
+<head>
+  <title>My A-Frame Scene</title>
+  <script src="https://cdn.rawgit.com/aframevr/aframe/master/dist/aframe.min.js"></script>
+  <script src="https://rawgit.com/aframe-vive-cursor-component/master/dist/aframe-vive-cursor-component.min.js"></script>
+</head>
+
+<body>
+  <a-scene>
+    <a-entity vive-controls="hand: left"></a-entity>
+    <a-entity vive-controls="hand: right" vive-cursor></a-entity>
+  </a-scene>
+</body>
+```
+
+#### npm
+
+Install via npm:
+
+```bash
+npm install aframe-vive-cursor-component
+```
+
+Then register and use.
+
+```js
+require('aframe');
+require('aframe-vive-cursor-component');
+```
+
---
+++ index.js
@@ -0,0 +1,203 @@
+/* global AFRAME, THREE */
+
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
+
+var EVENTS = {
+  CLICK: 'click',
+  MOUSEENTER: 'mouseenter',
+  MOUSEDOWN: 'mousedown',
+  MOUSELEAVE: 'mouseleave',
+  MOUSEUP: 'mouseup'
+};
+
+var STATES = {
+  HOVERING: 'cursor-hovering',
+  HOVERED: 'cursor-hovered'
+};
+
+/**
+ * Vive cursor component. A modification of the default cursor.
+ * Cursor can be fine-tuned by setting raycaster properties.
+ *
+ * @member {Element} mouseDownEl - Entity that was last mousedowned during current click.
+ * @member {Element} intersectedEl - Currently-intersected entity. Used to keep track to
+ *         emit events when unintersecting.
+ */
+AFRAME.registerComponent('vive-cursor', {
+  dependencies: ['raycaster'],
+  schema: {
+    color: {
+      type: 'color',
+      default: 0x0000ff
+    },
+    radius: {
+      type: 'number',
+      default: '0.001'
+    }
+  },
+
+  /**
+   * Set if component needs multiple instancing.
+   */
+  multiple: false,
+
+  /**
+   * Called once when component is attached. Generally for initial setup.
+   */
+  init: function () {
+    var cursorEl = this.el;
+    var data = this.data;
+    var canvas = cursorEl.sceneEl.canvas;
+    this.mouseDownEl = null;
+    this.intersectedEl = null;
+
+    // Wait for canvas to load.
+    if (!canvas) {
+      cursorEl.sceneEl.addEventListener('render-target-loaded', this.init.bind(this));
+      return;
+    }
+
+    // Create laser beam.
+    var cursorGeometry = new THREE.CylinderGeometry(data.radius, data.radius, 1000, 32);
+    var cursorMaterial = new THREE.MeshBasicMaterial({color: data.color});
+    var cursorMesh = new THREE.Mesh(cursorGeometry, cursorMaterial);
+    // Move mesh so that beam starts at the tip of the controller model.
+    cursorMesh.position.z = -500;
+    // Rotate mesh so that it points directly away from the controller.
+    cursorMesh.rotation.x = 90 * (Math.PI / 180);
+    this.el.setObject3D('vive-cursor-mesh', cursorMesh);
+
+    // Prevent laser beam from interfering with raycaster.
+    cursorEl.setAttribute('raycaster', 'near: 0.03');
+
+    // Save event listener bindings (needed for removal).
+    this.onIntersectionBind = this.onIntersection.bind(this);
+    this.onIntersectionClearedBind = this.onIntersectionCleared.bind(this);
+    this.onMouseDownBind = this.onMouseDown.bind(this);
+    this.onMouseUpBind = this.onMouseUp.bind(this);
+
+    // Attach event listeners.
+    this.attachEventListeners();
+  },
+
+  attachEventListeners: function () {
+    var cursorEl = this.el;
+
+    cursorEl.addEventListener('raycaster-intersection', this.onIntersectionBind);
+    cursorEl.addEventListener('raycaster-intersection-cleared', this.onIntersectionClearedBind);
+    // Mouseup/down mapped to trigger.
+    cursorEl.addEventListener('triggerdown', this.onMouseDownBind);
+    cursorEl.addEventListener('triggerup', this.onMouseUpBind);
+  },
+
+  removeEventListeners: function () {
+    var cursorEl = this.el;
+
+    cursorEl.removeEventListener('raycaster-intersection', this.onIntersectionBind);
+    cursorEl.removeEventListener('raycaster-intersection-cleared', this.onIntersectionClearedBind);
+    cursorEl.removeEventListener('triggerdown', this.onMouseDownBind);
+    cursorEl.removeEventListener('triggerup', this.onMouseUpBind);
+  },
+
+  /**
+   * Trigger mousedown and keep track of the mousedowned entity.
+   */
+  onMouseDown: function (evt) {
+    this.twoWayEmit(EVENTS.MOUSEDOWN);
+    this.mouseDownEl = this.intersectedEl;
+  },
+
+  /**
+   * Trigger mouseup if:
+   * - Currently intersecting an entity.
+   * - Currently-intersected entity is the same as the one when mousedown was triggered,
+   *   in case user mousedowned one entity, dragged to another, and mouseupped.
+   */
+  onMouseUp: function () {
+    this.twoWayEmit(EVENTS.MOUSEUP);
+    if (!this.intersectedEl || this.mouseDownEl !== this.intersectedEl) { return; }
+    this.twoWayEmit(EVENTS.CLICK);
+  },
+
+  /**
+   * Handle intersection.
+   */
+  onIntersection: function (evt) {
+    var self = this;
+    var cursorEl = this.el;
+    var intersectedEl = evt.detail.els[0];  // Grab the closest.
+
+    // Set intersected entity if not already intersecting.
+    if (this.intersectedEl === intersectedEl) { return; }
+    this.intersectedEl = intersectedEl;
+
+    // Hovering.
+    cursorEl.addState(STATES.HOVERING);
+    intersectedEl.addState(STATES.HOVERED);
+    self.twoWayEmit(EVENTS.MOUSEENTER);
+  },
+
+  /**
+   * Handle intersection cleared.
+   */
+  onIntersectionCleared: function (evt) {
+    var cursorEl = this.el;
+    var intersectedEl = evt.detail.el;
+
+    // Not intersecting.
+    if (!intersectedEl || !this.intersectedEl) { return; }
+
+    // No longer hovering.
+    intersectedEl.removeState(STATES.HOVERED);
+    cursorEl.removeState(STATES.HOVERING);
+    this.twoWayEmit(EVENTS.MOUSELEAVE);
+
+    // Unset intersected entity (after emitting the event).
+    this.intersectedEl = null;
+  },
+
+  /**
+   * Helper to emit on both the cursor and the intersected entity (if exists).
+   */
+  twoWayEmit: function (evtName) {
+    var intersectedEl = this.intersectedEl;
+    this.el.emit(evtName, {intersectedEl: this.intersectedEl});
+    if (!intersectedEl) { return; }
+    intersectedEl.emit(evtName, {cursorEl: this.el});
+  },
+
+  /**
+   * Called when a component is removed (e.g., via removeAttribute).
+   * Generally undoes all modifications to the entity.
+   */
+  remove: function () {
+    var cursorEl = this.el;
+
+    // Remove laser beam mesh.
+    cursorEl.removeObject3D('vive-cursor-mesh');
+
+    // Remove event listeners.
+    this.removeEventListeners();
+
+    // Remove raycaster.
+    cursorEl.removeAttribute('raycaster');
+  },
+
+  /**
+   * Called when entity pauses.
+   * Use to stop or remove any dynamic or background behavior such as events.
+   */
+  pause: function () {
+    this.removeEventListeners();
+  },
+
+  /**
+   * Called when entity resumes.
+   * Use to continue or add any dynamic or background behavior such as events.
+   */
+  play: function () {
+    this.attachEventListeners();
+  }
+});
